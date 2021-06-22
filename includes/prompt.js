43cf834b6c0233d6b1d05a95d373d0ab522f6169
Syed MH <hassan@syedmh.com>
2021-06-22 23:20:48 +0500

    Refactored the entire app so far into separate files

--- includes/prompt.js
+++ includes/prompt.js
@@ -2,12 +2,11 @@
  * This is a function that runs a recursive prompt
  * that asks the user to input URLS to analyze.
  *
- * @todo add validation to see if something is actually
- * a URL
- *
+ * @todo add validation to see if something is actually a URL
+ * @param { boolean } VALIDATE
  * @return { string[] } Array of trimmed urls
  */
-module.exports = () => {
+module.exports = (VALIDATE) => {
   /**
    * @const { {} } CONSTANTS Object containing app-wide string constants
    * @const { module } prompt prompt-async library
@@ -18,7 +17,7 @@ module.exports = () => {
    * @var { Number } _voided The number of URLs not added
    * @var { Number } _total The total number of URLs that is going to be processed
    */
-  const CONSTANTS = require("./constants");
+  const CONSTANTS = require("./utilities/constants");
   const PROMPT = require("prompt-sync")({ sigint: true });
   let _urls = new Array();
   let _input, _add, _added, _voided, _total;
@@ -32,10 +31,20 @@ module.exports = () => {
     _input = PROMPT()
       .split(",")
       .map((_url) => _url.trim());
-    _add = _input.filter((_url) => !_urls.contains(_url) && _url);
+    _add = _input.filter((_url) => !_urls.includes(_url) && _url);
     _added = _add.length;
     _voided = _input.length - _added;
     _total = _urls.length;
+    _urls = [..._urls, ..._add];
+    console.log(
+      CONSTANTS.COLORS.GREEN,
+      CONSTANTS.URLS_ADDED(_added),
+      CONSTANTS.COLORS.GREEN,
+      CONSTANTS.TOTAL_URLS(_total),
+      CONSTANTS.COLORS.RED,
+      CONSTANTS.DUPLICATES_NOT_ADDED(_voided),
+      CONSTANTS.COLORS.RESET
+    );
   } while (_input.join("") !== "");
 
   return _urls;
--- includes/utilities/constants.js
+++ includes/utilities/constants.js
@@ -3,17 +3,17 @@ module.exports = {
     "Enter another URL or a comma separated string of URLs to use, or press enter to finish adding domains:",
   ENTER_ANOTHER_URL: "Enter a url or a comma seperated string of URLs to use:",
   /**
-   * @param { int } NOT_ADDED
+   * @param { Number } NOT_ADDED
    * @returns { string }
    */
   DUPLICATES_NOT_ADDED: (NOT_ADDED) => `${NOT_ADDED} duplicates not added`,
   /**
-   * @param { int } ADDED
+   * @param { Number } ADDED
    * @returns { string }
    */
   URLS_ADDED: (ADDED) => `${ADDED} URLs added`,
   /**
-   * @param { int } TOTAL
+   * @param { Number } TOTAL
    * @returns { string }
    */
   TOTAL_URLS: (TOTAL) => `${TOTAL} total URLs`,
@@ -22,7 +22,7 @@ module.exports = {
    * @returns { string }
    */
   LIST_URLS: (URLS) =>
-    `The following URLs will now be processed:\n${URLS.join("\n- ")}`,
+    `The following URLs will now be processed:\n- ${URLS.join("\n- ")}`,
   COLORS: {
     RED: "\x1b[31m",
     GREEN: "\x1b[32m",
